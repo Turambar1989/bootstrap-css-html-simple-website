5e46b666e57d608efafbf5213a79fec6fb0a4ae3
Turambar1989 <kukielka.daniel89@gmail.com>
2025-03-04 12:21:41 +0100

    Add files via upload

--- public/js-projects/Project3similar-DisplayElementsAndChangingSize/main.js
+++ public/js-projects/Project3similar-DisplayElementsAndChangingSize/main.js
@@ -5,18 +5,22 @@ const ulCreate = document.createElement("ul");
 
 const init = () => {
   const btn = document.createElement("button");
+  const btnReset = document.createElement("button");
   btn.textContent = "ClickMe";
   btn.style.marginBottom = 10 + "px";
   btn.classList.add("button");
+  btnReset.textContent = "Reset";
+  btnReset.classList.add("button");
   divContainer.appendChild(btn);
+  divContainer.appendChild(btnReset);
   ulCreate.style.textAlign = "center";
   ulCreate.style.marginTop = 15 + "px";
   ulCreate.style.listStyle = "none";
   ulCreate.style.display = "inline";
   divContainer.appendChild(ulCreate);
   btn.addEventListener("click", createLiElements);
+  btnReset.addEventListener("click", resetList);
 };
-
 const createLiElements = () => {
   for (let i = 0; i < 10; i++) {
     const liCreate = document.createElement("li");
@@ -29,6 +33,11 @@ const createLiElements = () => {
     ulCreate.appendChild(liCreate);
     console.log(liCreate);
   }
+};
+const resetList = () => {
+  ulCreate.innerHTML = "";
+  size = 10;
+  orderElement = 1;
 };
 
 init();
